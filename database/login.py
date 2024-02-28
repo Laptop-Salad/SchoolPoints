43f99dcc396963e6538f12701e648660a09c7019
30211196 <30211196@nescol.ac.uk>
2024-02-28 15:27:10 +0000

    ...

--- database/login.py
+++ database/login.py
@@ -6,86 +6,6 @@ conn = sqlite3.connect("school.db")
 cursor = conn.cursor()
 
 def studentLogin():
-    #get username
-    username = input("Enter username: ")
-    username = "'"+ username+ "'"
-
-    #look for username is database and get ID
-    cursor.execute("SELECT id FROM students WHERE username = %s" %(username))
-    check = cursor.fetchall()
-    if str(check) == "[]" :
-        #if username isn't in the database
-        print("username incorrect")
-    else:
-        #if username is in the databse
-        check =  str(check [0])
-        check = check[1:2]
-
-        #get password
-        password = input("Enter password: ")
-        #get password from the same row as the ID of username in database
-        cursor.execute("SELECT password FROM students WHERE id = %s" %(check))
-        check1 = cursor.fetchall()
-        check1 =  str(check1 [0])
-        num = len(check1) - 3
-        check1 = check1[2:num]
-        if check1 == password:
-            #password in database is the same as the one entered
-            print("Password correct go to logged in page")
-        else:
-            #password is different
-            print("Password incorrect")
-        return None
-
-def teacherLogin():
-    username = input("Enter username: ")
-    username = "'"+ username+ "'"
-
-    cursor.execute("SELECT id FROM teachers WHERE username = %s" %(username))
-    check = cursor.fetchall()
-    if str(check) == "[]" :
-        print("username incorrect")
-    else:
-        check =  str(check [0])
-        check = check[1:2]
-
-        password = input("Enter password: ")
-        cursor.execute("SELECT password FROM teachers WHERE id = %s" %(check))
-        check1 = cursor.fetchall()
-        check1 =  str(check1 [0])
-        num = len(check1) - 3
-        check1 = check1[2:num]
-        if check1 == password:
-            print("Password correct go to logged in page")
-        else:
-            print("Password incorrect")
-        return None
-
-def parentLogin():
-    username = input("Enter username: ")
-    username = "'"+ username+ "'"
-
-    cursor.execute("SELECT id FROM parents WHERE username = %s" %(username))
-    check = cursor.fetchall()
-    if str(check) == "[]" :
-        print("username incorrect")
-    else:
-        check =  str(check [0])
-        check = check[1:2]
-
-        password = input("Enter password: ")
-        cursor.execute("SELECT password FROM parents WHERE id = %s" %(check))
-        check1 = cursor.fetchall()
-        check1 =  str(check1 [0])
-        num = len(check1) - 3
-        check1 = check1[2:num]
-        if check1 == password:
-            print("Password correct go to logged in page")
-        else:
-            print("Password incorrect")
-    return None
-
-def studentLogin2():
     username = input("Enter username: ")
     password = input("Enter Password: ")
 
@@ -101,7 +21,7 @@ def studentLogin2():
         check = check[1:2]
         print("Id is" , check)
 
-def parentLogin2():
+def parentLogin():
     username = input("Enter username: ")
     password = input("Enter Password: ")
 
@@ -117,7 +37,7 @@ def parentLogin2():
         check = check[1:2]
         print("Id is", check)
 
-def teacherLogin2():
+def teacherLogin():
     username = input("Enter username: ")
     password = input("Enter Password: ")
 
@@ -133,8 +53,8 @@ def teacherLogin2():
         check = check[1:2]
         print("Id is" , check)
 
-studentLogin2()
-parentLogin2()
-teacherLogin2()
+studentLogin()
+parentLogin()
+teacherLogin()
 
 
