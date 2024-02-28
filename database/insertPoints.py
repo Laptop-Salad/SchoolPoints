f3c2a251d1e5cf0ac604558a81c1069589c69da6
AMANDA WALLIS <30132460@computing.nescol.ac.uk>
2024-02-28 15:48:06 +0000

    add student dashboard

--- database/insertPoints.py
+++ database/insertPoints.py
@@ -13,7 +13,7 @@ cursor = conn.cursor()
 
 # Changed points db structure in db.py
 
-
+# Behavior points
 # cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
 # VALUES (1, 1, 'Is very quiet and doesnt ask for my help or attention ever', 10, 'B');
 #
@@ -23,6 +23,43 @@ cursor = conn.cursor()
 # VALUES(1, 2, 'Doesnt sit still and not speak', -10, 'B');
 #
 # """)
+
+
+### Grades points
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES (1, 1, 'Doesnt know what a dog is but otherwise very good student', 20, 'G');
+##
+##""")
+##
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES(1, 2, 'Is not too good at english', -5, 'G');
+##
+##""")
+##
+##
+### Attendance points
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES (1, 1, 'Always comes to class on time', 10, 'A');
+##
+##""")
+##
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES(1, 2, 'Was 34.30 minutes late to class today', -34.3, 'A');
+##
+##""")
+##
+##
+### Others points
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES (1, 1, 'Smiled at me', 100, 'O');
+##
+##""")
+##
+##cursor.execute("""INSERT INTO points(studentid, teacherid, comment, points, pointsTo)
+##VALUES(1, 2, 'Didnt smile at me', -1, 'O');
+##
+##""")
+
 
 print("Records added to Database")
 conn.commit()
