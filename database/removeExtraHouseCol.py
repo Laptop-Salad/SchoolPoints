28f00f0cad0707c563e8af911d1c9982e8d86d7a
Amanda Wallis <awallis1609@gmail.com>
2024-03-28 23:56:03 +0000

    remove houseid col from students

---
+++ database/removeExtraHouseCol.py
@@ -0,0 +1,10 @@
+# import sqlite3
+# from sqlite3 import Error
+
+# conn = sqlite3.connect("school.db")
+# cursor = conn.cursor()
+
+# cursor.execute("ALTER TABLE students DROP COLUMN houseid")
+
+# conn.commit()
+# conn.close()
