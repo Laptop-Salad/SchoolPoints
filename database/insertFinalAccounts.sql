2c94d3133dd99d9d4db985584ecdd4acad5332c0
Amanda Wallis <awallis1609@gmail.com>
2024-05-04 18:32:14 +0100

    add new student accounts

---
+++ database/insertFinalAccounts.sql
@@ -0,0 +1,173 @@
+-- ** 5 Green Plants Students **
+
+-- 1: Megan
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Megan", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+
+INSERT INTO houses (studentid, housename) 
+VALUES (1, "Green Plants");
+
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Megan Mom", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 1);
+
+-- 2 : Sam
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Sam", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+
+INSERT INTO houses (studentid, housename) 
+VALUES (2, "Green Plants");
+
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Sam Dad", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 2);
+
+-- 3: Derrick
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Derrick", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+
+INSERT INTO houses (studentid, housename) 
+VALUES (3, "Green Plants");
+
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Derrick Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 3);
+
+-- 3: SamuelBrown
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("SamuelBrown", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (4, "Green Plants");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("SamuelBrownParent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 4);
+
+-- 4: DerrickSanders
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("DerrickSanders", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (5, "Green Plants");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("DerrickSanders Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 5);
+
+-- ** 5 Red Rabbits Students **
+
+-- 1: Serena
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Serena", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (6, "Red Rabbits");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Serena Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 6);
+
+-- 2: Max
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Max", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (7, "Red Rabbits");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Max Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 7);
+
+-- 3: Amelia
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Amelia", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (8, "Red Rabbits");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Amelia Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 8);
+
+-- 4: Carmex
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Carmex", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (9, "Red Rabbits");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Carmex Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 9);
+
+-- 5: Anker
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Anker", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (10, "Red Rabbits");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Anker Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 10);
+
+-- ** 5 Blue Ocean Students **
+
+-- 1: Octopus
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Octopus", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (11, "Blue Ocean");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Octopus Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 11);
+
+-- 2: Jared
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Jared", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (12, "Blue Ocean");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Jared Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 12);
+
+-- 3: Jared
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Jared", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (13, "Blue Ocean");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Jared Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 13);
+
+-- 4: Arizona
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Arizona", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (14, "Blue Ocean");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Arizona Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 14);
+
+-- 5: Tragedeigh
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Tragedeigh", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (15, "Blue Ocean");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Tragedeigh Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 15);
+
+-- ** 5 Yellow Sun Students **
+
+-- 1: Almond
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Almond", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (16, "Yellow Sun");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Almond Mom", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 16);
+
+-- 2: Iron Man
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Iron Man", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (17, "Yellow Sun");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Iron Man Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 17);
+
+-- 3: Philosophy
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Philosophy", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (18, "Yellow Sun");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Philosophy Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 18);
+
+-- 4: Djundjungelskog
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Djundjungelskog", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (19, "Yellow Sun");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Djundjungelskog Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 19);
+
+-- 5: Veronica
+INSERT INTO students (username, password, resetpassword) 
+VALUES ("Veronica", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 0);
+INSERT INTO houses (studentid, housename) 
+VALUES (20, "Yellow Sun");
+INSERT INTO parents (username, password, studentid) 
+VALUES ("Veronica Parent", "$2y$10$zxnJ4w7hp0BLKuISOfrvmexUaeDCQsQWjej2/l2JlVZl7Rb.HO9eW", 20);
